dc1f0618fd010dd7b2ddfcc9360375b0e5a46d8d
Di Wu <z5156324@student.unsw.edu.au>
2022-07-03 22:59:58 +1000

    q15

--- main.py
+++ main.py
@@ -15,46 +15,46 @@ travel_file = "340101.xlsx"
 #  data cleansing part
 df = data_cleansing(count_file, rain_file, temp_file, solar_file)
 
-# # answer for question 1
-# df_q1 = pedestrianStats(df, 2022, [8, 13, 17])
-# print(df_q1)
+# answer for question 1
+df_q1 = pedestrianStats(df, 2022, [8, 13, 17])
+print(df_q1)
 
-# # answer for question 2
-# pedestrianScatter(df, 2021, 2022, 'Maximum temperature (Degree C)')
+# answer for question 2
+pedestrianScatter(df, 2021, 2022, 'Maximum temperature (Degree C)')
 
-# # answer for question 3
-# pedestrianScatter(df, 2021, 2022, 'Rainfall amount (millimetres)')
+# answer for question 3
+pedestrianScatter(df, 2021, 2022, 'Rainfall amount (millimetres)')
 
-# # answer for question 4
-# pedestrianScatter(df, 2021, 2022, 'Daily global solar exposure (MJ/m*m)')
+# answer for question 4
+pedestrianScatter(df, 2021, 2022, 'Daily global solar exposure (MJ/m*m)')
 
-# #answer for question 5
-# pedestrianHist(df, 2021, 2022)
+#answer for question 5
+pedestrianHist(df, 2021, 2022)
 
-# #answer for question 6
-# sensorHist(df, 2022, 1, 20)
+#answer for question 6
+sensorHist(df, 2022, 1, 20)
 
-# #answer for question 7
-# pedestrianHistRain(df, 2021, 2022)
+#answer for question 7
+pedestrianHistRain(df, 2021, 2022)
 
-# #answer for question 8
-# pedestrianHistRainTemp(df, 2021, 2022, 20)
+#answer for question 8
+pedestrianHistRainTemp(df, 2021, 2022, 20)
 
-# #answer for question 9
-# timeSeriesSensor(df, 2021, 2022, 'May')
+#answer for question 9
+timeSeriesSensor(df, 2021, 2022, 'May')
 
-# #answer for question 10
-# result = modelForCount(df, 3, 12, 13)
-# print(result)
+#answer for question 10
+result = modelForCount(df, 3, 12, 13)
+print(result)
 
-# #answer for question 11
-# unusualDay(df, 3)
+#answer for question 11
+unusualDay(df, 3)
 
-# # answer for question 12
-# dailyDifference(df, 3, 9)
+# answer for question 12
+dailyDifference(df, 3, 9)
 
-# # answer for question 13
-# sensorCorrelation(df, 3, 9)
+# answer for question 13
+sensorCorrelation(df, 3, 9)
 
 #answer for question 15
 investCovidTravel(df, covid_file, travel_file)
